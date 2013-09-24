e2fbe90b94abfe03d8a6e77a000bdcb64c9a8765
Czara <Christophe@MacBook-Pro-de-Christophe.local>
2013-09-24 21:58:35 +0200

    Tests encodage

--- tvsamsung.js
+++ tvsamsung.js
@@ -9,41 +9,31 @@ exports.action = function(data, callback, cfg, SARAH){
     return;
   }
 
-	var chr = function(asciiNum)
-	{
-		return String.fromCharCode (asciiNum);
-	}
+  var chr = function(asciiNum){
+    return String.fromCharCode (asciiNum);
+  }
 	
-  var tvappstring = "iphone.UE55C8000.iapp.samsung"
-  var appstring = "iphone..iapp.samsung";
-  var remotename = "Python Samsung Remote";
+  var tvappstring = "sarah.UE55C8000.iapp.samsung";
+  var appstring = "sarah..iapp.samsung";
+  var remotename = "SARAH";
 
   //Function to send Key command
   var sendKey = function(skey, socket, appstring){
     var skeyBuff  = new Buffer(skey).toString('base64');
-    var msgPart3  = chr(0x00)+chr(0x00)+chr(0x00)+strBuff(skeyBuff);
+    var msgPart3    = chr(0x00) 
+                    + chr(0x00)
+                    + chr(0x00)
+                    + strBuff(skeyBuff);
 
-    var footPart3 = chr(0x00)+strBuff(appstring)+strBuff(msgPart3);
+    var footPart3   = chr(0x00) 
+                    + strBuff(appstring) 
+                    + strBuff(msgPart3);
 
     socket.write(footPart3);
+    
 	console.log('TRoISIEME PARTIE DU MESSAGE: ' + msgPart3);
 	console.log('TRoISIEME PARTIE DU MESSAGE: ' + footPart3);
   }
-
-  //Function to send message
-  /* var sendMessage = function(message, socket, appstring){
-    var messageBuff  = new Buffer(message).toString('base64');
-    var msgPart3  = 0x01 
-                  + 0x00
-                  + strBuff(messageBuff)
-
-    var footPart3 = 0x01
-                  + strBuff(tvappstring)
-                  + strBuff(msgPart3)
-
-    socket.write(footPart3);
-	
-  } */
   
   var strBuff = function(param){
 	var len = param.length;
@@ -72,34 +62,41 @@ exports.action = function(data, callback, cfg, SARAH){
     console.log('CONNECTED TO: ' + config.tvip + ':' + 55000);
     
     var ipencoded  = new Buffer(config.tvip).toString('base64');
-    var macencoded = new Buffer("dc-71-44-2a-76-4f").toString('base64'); // 00-0c-29-3e-b1-4f
+    //var macencoded = new Buffer("dc-71-44-2a-76-4f").toString('base64'); // 00-0c-29-3e-b1-4f
+      var macencoded = new Buffer("0").toString('base64');
     var rnencoded = new Buffer(remotename).toString('base64');
     
     // Sending Part 1
-    var msgPart1  = chr(0x64)+chr(0x00)+chr(ipencoded.length)+chr(0x00)+ipencoded+chr(macencoded.length)+chr(0x00)+macencoded+chr(rnencoded)+chr(0x00)+rnencoded;
-	//var msgPart1  = chr(0x64)+chr(0x00)+strBuff(ipencoded)+strBuff(macencoded)+strBuff(remotename);
+    var msgPart1    = chr(0x64)
+                    + chr(0x00)
+                    + chr(ipencoded.length)
+                    + chr(0x00)
+                    + ipencoded
+                    + chr(macencoded.length) 
+                    + chr(0x00)
+                    + macencoded
+                    + chr(rnencoded.length)
+                    + chr(0x00)
+                    + rnencoded;
+	//var msgPart1  = chr(0x64)+chr(0x00)+strBuff(ipencoded)+strBuff(macencoded)+strBuff(rnencoded);
 	
-    var footPart1 = chr(0x00)+strBuff(appstring)+strBuff(msgPart1);
+    var footPart1   = chr(0x00)
+                    + strBuff(appstring)
+                    + strBuff(msgPart1);
+      
     socket.write(footPart1);
-	
-	console.log('0x00: ' + chr(0x00));
-	console.log('0x64: ' + chr(0x64));
-	console.log('0xc8: ' + chr(0xc8));
-	console.log('longueur de ipencoded : ' + ipencoded.length);
-	console.log('valeur de ipencoded : ' + ipencoded);
-	console.log('chr de longueur de ipencoded : '+ chr(ipencoded.length));
-	console.log('chr de 16 : ' + chr(16));
-	var ipBuff  = new Buffer(ipencoded.length, encoding='utf8');
-	console.log ('IPBuff : ' + ipBuff);
 
 	console.log('PREMIERE PARTIE DU MESSAGE: ' + msgPart1);
 	console.log('PREMIERE PARTIE DU MESSAGE: ' + footPart1);
     
     
     // Sending Part 2
-    var msgPart2  = chr(0xc8)+chr(0x00);
+    var msgPart2    = chr(0xc8)
+                    + chr(0x00);
                   
-    var footPart2 = chr(0x00)+strBuff(appstring)+strBuff(msgPart2);
+    var footPart2   = chr(0x00) 
+                    + strBuff(appstring)
+                    + strBuff(msgPart2);
                   
     socket.write(footPart2);
 	console.log('DEUXIEME PARTIE DU MESSAGE: ' + msgPart2);
@@ -110,9 +107,6 @@ exports.action = function(data, callback, cfg, SARAH){
     sendKey("KEY_VOLUP", socket, tvappstring);
 	//console.log(data.key);
 	//callback({ 'tts' : "Voilà qui est fait" })
-
-    // Sending message
-    //sendMessage("HELLO WORLD", socket, tvappstring);
       
     // Close the client socket completely
     socket.destroy();
